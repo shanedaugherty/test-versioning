260a35c5e8cb68da183305f4e9a52d9159754e0a
shanedaugherty <shane.daugherty@ilanthealth.com>
2024-10-01 06:16:57 -0600

    feat: remove thing why is it not working

--- release.config.js
+++ release.config.js
@@ -1,49 +1,56 @@
+const appPackageJsons = require('./tools/lib-names').map(
+    (pkg) => `./libs/${pkg}/package.json`
+)
+
+const libPackageJsons = require('./tools/app-names').map(
+    (pkg) => `./apps/${pkg}/package.json`
+)
+
+console.log({appPackageJsons, libPackageJsons})
+
+
 module.exports = {
-  pkgRoot: `./`,
-  tagFormat: 'v${version}',
-  debug: true,
-  ci: true,
-  branches: [
-    'main',
-    {
-      name: 'staging',
-      prerelease: true
-    }
-  ],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md'
-      }
+    pkgRoot: `./`,
+    tagFormat: 'v${version}',
+    debug: true,
+    ci: true,
+    branches: [
+        'main',
+        {
+            name: 'staging',
+            prerelease: true
+        }
     ],
-    '@semantic-release/npm',
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd:
-          'yarn generate-build-numbers && yarn sync-version-numbers && git stage .'
-      }
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: [
-          'package.json',
-          ...require('./tools/lib-names').map(
-            (pkg) => `./libs/${pkg}/package.json`
-          ),
-          ...require('./tools/app-names').map(
-            (pkg) => `./apps/${pkg}/package.json`
-          ),
-          'yarn.lock',
-          '**/android-version-code.json',
-          'CHANGELOG.md'
-        ]
-      }
-    ],
-    '@semantic-release/github'
-  ]
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md'
+            }
+        ],
+        '@semantic-release/npm',
+        [
+            '@semantic-release/exec',
+            {
+                prepareCmd:
+                    'yarn generate-build-numbers && yarn sync-version-numbers && git stage .'
+            }
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: [
+                    'package.json',
+                    ...appPackageJsons,
+                    ...libPackageJsons,
+                    'yarn.lock',
+                    '**/android-version-code.json',
+                    'CHANGELOG.md'
+                ]
+            }
+        ],
+        '@semantic-release/github'
+    ]
 };
